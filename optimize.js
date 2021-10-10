0e80b3dcd2d39428f3edfa7ec4451661a8b23886
Paulo Messias <paulo@192.168.1.7>
2021-10-10 14:47:20 -0300

    push commits to master

---
+++ optimize.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const AWS = require('aws-sdk');
+const sharp = require('sharp');
+const { basename, extname } = require('path');
+
+const S3 = new AWS.S3();
+
+module.exports.run = async ({ Records: records }, context) => {
+  try {
+    await Promise.all(records.map(async record => {
+      const { key } = record.s3.object;
+
+      const image = await S3.getObject({
+        Bucket: process.env.bucket,
+        Key: key
+      }).promise();
+
+      const optimized = await sharp(image.Body)
+        .resize(1920, 1080, { fit: 'inside', withoutEnlargement: true })
+        .toFormat('jpeg', { progressive: true, quality: 50 })
+        .toBuffer()
+
+      await S3.putObject({
+        Body: optimized,
+        Bucket: process.env.bucket,
+        ContentType: 'image/jpeg',
+        Key: `compressed/${basename(key, extname(key))}.jpeg`,
+      }).promise()
+
+      return {
+        statusCode: 301,
+        body: {}
+      }
+    }))
+  } catch (err) {
+    return err;
+  }
+};
